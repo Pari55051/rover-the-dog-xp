d53860150e6ee2786594dd191a17518e5112e039
Pari55051 <xpari5505@gmail.com>
2025-08-13 00:38:14 +0530

    added a random surprise animation after idle

--- program.cs
+++ program.cs
@@ -3,103 +3,153 @@ using System.Drawing;
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
-using System.Collections.Generic;
 
-class RoverForm : Form
+class RoverApp : Form
 {
-    private PictureBox roverBox;
-    private Point mouseOffset;
-    private bool isMouseDown = false;
-    private Timer animationTimer;
-    private Random rand = new Random();
+    PictureBox roverBox;
+    Random rand = new Random();
+    Timer idleTimer;
+    Timer surpriseTimer;
+    bool isMouseDown;
+    Point mouseOffset;
 
-    private string[] gifNames = new string[]
-    {
-        "paw-dabbing.gif",
-        "sniffing.gif",
-        "rover-football.gif",
-        "reading.gif",
-        "reading-red.gif",
-        "picture-of-him.gif",
-        "newspaper.gif",
-        "moviestar.gif",
-        "masterchef.gif",
-        "licks-screen.gif",
-        "jumps-away.gif",
-        "idle-sleep.gif",
+    string[] idleGifs = {
         "idle.gif",
+        "idle-sleep.gif",
+        "sniffing.gif",
         "waiting.gif",
-        "digging-success.gif",
-        "digging.gif",
-        "drops-bag.gif",
-        "drops-guest-bag.gif",
-        "excited-play.gif"
+        "paw-dabbing.gif"
     };
 
-    public RoverForm()
+    string[] surpriseGifs = {
+        "digging.gif",
+        "digging-success.gif",
+        "drops-bag.gif",
+        "drops-guest-bag.gif",
+        "jumps-away.gif"
+    };
+
+    public RoverApp()
     {
-        // Form settings
         this.FormBorderStyle = FormBorderStyle.None;
         this.TopMost = true;
         this.BackColor = Color.Lime;
         this.TransparencyKey = Color.Lime;
         this.StartPosition = FormStartPosition.CenterScreen;
 
-        // PictureBox for Rover
         roverBox = new PictureBox();
-        roverBox.Image = LoadGifFromResources("idle.gif"); // start idle
         roverBox.BackColor = Color.Lime;
         roverBox.SizeMode = PictureBoxSizeMode.AutoSize;
+        roverBox.Image = LoadGifFromResources("idle.gif");
+        roverBox.MouseDown += RoverBox_MouseDown;
+        roverBox.MouseMove += RoverBox_MouseMove;
+        roverBox.MouseUp += RoverBox_MouseUp;
+        roverBox.ContextMenuStrip = BuildContextMenu();
         this.Controls.Add(roverBox);
 
-        // Drag support
-        roverBox.MouseDown += (s, e) =>
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                isMouseDown = true;
-                mouseOffset = new Point(-e.X, -e.Y);
-            }
-        };
-        roverBox.MouseMove += (s, e) =>
-        {
-            if (isMouseDown)
-            {
-                Point mousePos = Control.MousePosition;
-                mousePos.Offset(mouseOffset.X, mouseOffset.Y);
-                this.Location = mousePos;
-            }
-        };
-        roverBox.MouseUp += (s, e) => { isMouseDown = false; };
+        idleTimer = new Timer();
+        idleTimer.Interval = rand.Next(5000, 8000);
+        idleTimer.Tick += OnIdleTimerTick;
+        idleTimer.Start();
 
-        // Animation timer (change every 5–10 seconds)
-        animationTimer = new Timer();
-        animationTimer.Interval = rand.Next(5000, 10000); // random interval
-        animationTimer.Tick += (s, e) =>
+        surpriseTimer = new Timer();
+        surpriseTimer.Interval = rand.Next(20000, 40000);
+        surpriseTimer.Tick += OnSurpriseTimerTick;
+        surpriseTimer.Start();
+    }
+
+    private void OnIdleTimerTick(object sender, EventArgs e)
+    {
+        PlayRandomIdle();
+        idleTimer.Interval = rand.Next(5000, 8000);
+    }
+
+    private void OnSurpriseTimerTick(object sender, EventArgs e)
+    {
+        PlayRandomSurprise();
+        surpriseTimer.Interval = rand.Next(20000, 40000);
+    }
+
+    private void RoverBox_MouseDown(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Left)
         {
-            string gif = gifNames[rand.Next(gifNames.Length)];
-            roverBox.Image = LoadGifFromResources(gif);
-            animationTimer.Interval = rand.Next(5000, 10000);
-        };
-        animationTimer.Start();
+            isMouseDown = true;
+            mouseOffset = new Point(-e.X, -e.Y);
+        }
+    }
+
+    private void RoverBox_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (isMouseDown)
+        {
+            Point mousePos = Control.MousePosition;
+            mousePos.Offset(mouseOffset.X, mouseOffset.Y);
+            this.Location = mousePos;
+        }
+    }
+
+    private void RoverBox_MouseUp(object sender, MouseEventArgs e)
+    {
+        isMouseDown = false;
+    }
+
+    private ContextMenuStrip BuildContextMenu()
+    {
+        var menu = new ContextMenuStrip();
+        menu.Items.Add("Play Football", null, delegate { PlayAction("rover-football.gif"); });
+        menu.Items.Add("Read Newspaper", null, delegate { PlayAction("newspaper.gif"); });
+        menu.Items.Add("Exit", null, delegate { Application.Exit(); });
+        return menu;
     }
 
     private Image LoadGifFromResources(string resourceName)
     {
-        var assembly = Assembly.GetExecutingAssembly();
-        using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+        var asm = Assembly.GetExecutingAssembly();
+        using (Stream stream = asm.GetManifestResourceStream(resourceName))
         {
             if (stream == null)
-                throw new Exception("Missing resource: " + resourceName);
+                throw new Exception("Resource not found: " + resourceName);
             return Image.FromStream(stream);
         }
     }
 
+    private void PlayRandomIdle()
+    {
+        string gif = idleGifs[rand.Next(idleGifs.Length)];
+        roverBox.Image = LoadGifFromResources(gif);
+    }
+
+    private void PlayRandomSurprise()
+    {
+        string gif = surpriseGifs[rand.Next(surpriseGifs.Length)];
+        roverBox.Image = LoadGifFromResources(gif);
+
+        var t = new Timer();
+        t.Interval = 3000;
+        t.Tick += delegate {
+            t.Stop();
+            PlayRandomIdle();
+        };
+        t.Start();
+    }
+
+    private void PlayAction(string gif)
+    {
+        roverBox.Image = LoadGifFromResources(gif);
+        var t = new Timer();
+        t.Interval = 3000;
+        t.Tick += delegate {
+            t.Stop();
+            PlayRandomIdle();
+        };
+        t.Start();
+    }
+
     [STAThread]
-    public static void Main()
+    static void Main()
     {
         Application.EnableVisualStyles();
-        Application.Run(new RoverForm());
+        Application.Run(new RoverApp());
     }
 }
-
